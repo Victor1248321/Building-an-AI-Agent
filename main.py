7017fa315f90108400aac16d7981b85e85ea81f9
Master-Magician <Marvin1248@hotmail.com>
2025-06-11 14:42:04 -0500

    --verbose Flag Added to See Additional Information on Output

--- main.py
+++ main.py
@@ -2,23 +2,41 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+
 if len(sys.argv) == 1:
     print("Error, Missing Prompt")
     sys.exit(1)
     
+is_verbose = False
 
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        is_verbose = True
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 
+if is_verbose == True:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+
+
+
+    
 
